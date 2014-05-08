1ac4ab116b380cdf784feac36161b65ce506cc8e
djwhiting <drwhiting@hotmail.com>
2014-05-09 00:08:57 +0100

    Added minerals and metals, but working on arrays

--- spaceclicker.js
+++ spaceclicker.js
@@ -1,6 +1,9 @@
 var version = "SpaceClicker v0.0.1";
 var dust = 0;
 var bricks = 0;
+var minerals = 0;
+var metal = 0;
+var stock = {"dust": 0,"bricks": 0,"minerals":0,"metal":0};
 
 console.log( version );
 
@@ -9,24 +12,33 @@ $( document ).ready(function() {
 	update();
 });
 
+function randomInt(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+};
+
+function showstock(item){
+	console.log([item]);
+	if ([item] > 0) {  $("#"+item).text([item] + " " + item); };
+};
 
 function update() {
-	if(dust < 5){
-		$("#compactlink").hide();
-	}
-	else {
-		$("#compactlink").show();
-	};
+	$("#dust").text(stock["dust"] + " dust");
+	showstock("bricks");
+	showstock("minerals");
+	showstock("metal");
 
-	$("#dust").text(dust + " dust ");
-	if (bricks > 0) {
-	  $("#bricks").text(bricks + " bricks ");
-	};
+	if(dust < 5){ $("#compactlink").hide();	} else { $("#compactlink").show(); };
+	if(minerals < 10){ $("#refinelink").hide(); } else { $("#refinelink").show(); };
 };
 
 
 function scrape(){	
-	dust = dust + 1;
+	stock["dust"] = stock["dust"] + 1;
+	var rnd = randomInt(1,5);
+    if (rnd==5) {
+    	minerals = minerals	+ 1;
+    };
 	update();
 };
 
@@ -34,4 +46,9 @@ function compact(){
 	dust = dust - 5;
 	bricks = bricks + 1;
 	update();
+};
+
+function refine(){
+	minerals = minerals - 10;
+	metal = metal + 1;
 };
