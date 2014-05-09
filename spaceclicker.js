3c5fa884a5bab9aa26da5f6a13cab76ef0ff9db4
djwhiting <drwhiting@hotmail.com>
2014-05-09 13:41:08 +0100

    Stash hash table working

--- spaceclicker.js
+++ spaceclicker.js
@@ -1,9 +1,5 @@
 var version = "SpaceClicker v0.0.1";
-var dust = 0;
-var bricks = 0;
-var minerals = 0;
-var metal = 0;
-var stock = {"dust": 0,"bricks": 0,"minerals":0,"metal":0};
+var stash = {dust:0,bricks:0,minerals:0,metal:0};
 
 console.log( version );
 
@@ -12,43 +8,42 @@ $( document ).ready(function() {
 	update();
 });
 
-function randomInt(min,max)
-{
+function randomInt(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 };
 
-function showstock(item){
-	console.log([item]);
-	if ([item] > 0) {  $("#"+item).text([item] + " " + item); };
-};
-
 function update() {
-	$("#dust").text(stock["dust"] + " dust");
-	showstock("bricks");
-	showstock("minerals");
-	showstock("metal");
+	$("#dust").text(stash["dust"] + " dust");
+	for(key in stash) {
+		if (stash[key] > 0) {
+		  $("#"+key).text(stash[key] + " " + key);
+		}
+		else {
+		  $("#"+key).text("");
+		};
+	};
 
-	if(dust < 5){ $("#compactlink").hide();	} else { $("#compactlink").show(); };
-	if(minerals < 10){ $("#refinelink").hide(); } else { $("#refinelink").show(); };
+	if(stash["dust"] < 5) {$("#compactlink").hide();} else {$("#compactlink").show();};
+	if(stash["minerals"] < 10) {$("#refinelink").hide();} else {$("#refinelink").show();};
 };
-
 
 function scrape(){	
-	stock["dust"] = stock["dust"] + 1;
+	stash["dust"] = stash["dust"] + 1;
 	var rnd = randomInt(1,5);
     if (rnd==5) {
-    	minerals = minerals	+ 1;
+		stash["minerals"] = stash["minerals"]	+ 1;
     };
 	update();
 };
 
 function compact(){
-	dust = dust - 5;
-	bricks = bricks + 1;
+	stash["dust"] = stash["dust"] - 5;
+	stash["bricks"] = stash["bricks"] + 1;
 	update();
 };
 
 function refine(){
-	minerals = minerals - 10;
-	metal = metal + 1;
+	stash["minerals"] = stash["minerals"] - 10;
+	stash["metal"] = stash["metal"] + 1;
+	update();
 };
