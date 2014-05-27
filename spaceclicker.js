cfc826baf468eebe6b82a7432614a492bf2ab462
djwhiting <drwhiting@hotmail.com>
2014-05-27 17:40:32 +0100

    Single update process
    Runs once per second.  Removed metal and robot stages

--- spaceclicker.js
+++ spaceclicker.js
@@ -1,11 +1,12 @@
 var version = "SpaceClicker v0.0.1";
-var stash = {dust:0,bricks:10,minerals:10,metal:0, robots:0, scrapers:0};
+var stash = {dust:0,bricks:0,minerals:0,scrapers:0};
 
 console.log( version );
 
 // Setup
 $( document ).ready(function() {
-	update();
+    setInterval(function () { automate(); }, 1000);
+    update();
 });
 
 function randomInt(min,max) {
@@ -14,7 +15,8 @@ function randomInt(min,max) {
 
 
 function update() {
-	var count = 0;
+    var count = 0;
+    // display stash
 	for(key in stash) {
 		count += stash[key];
 		if (stash[key] > 0) {
@@ -24,22 +26,24 @@ function update() {
 		  $("#"+key).text("");
 		};
 	};
+
+    // update buttons
 	if (count == 0) { $("#dust").text("nothing");};
 	$("#dust").append((" <a href='#' onClick='scrape()'>Scrape</a>"));
 	if (stash["dust"] >= 5) {
 		$("#dust").append((" <a href='#' onClick='compact()'>Compact (-5 dust)</a>"));
 		};
-	if (stash["minerals"] >= 10) {
-		$("#minerals").append((" <a href='#' onClick='refine()'> Refine (-10 minerals)</a>"));
-		};
-	if (stash["metal"] >= 1) {
-		$("#metal").append((" <a href='#' onClick='brobot()'> Build robot (-1 metal)</a>"));
-		};			
-	if ((stash["bricks"] >= 10) && (stash["robots"] >= 1)) {
-		$("#bricks").append((" <a href='#' onClick='bscraper()'> Build scraper (-10 bricks, -1 robot)</a>"));
-		};
-
+	if ((stash["bricks"] >= 10) && (stash["minerals"] >= 10)) {
+		$("#minerals").append((" <a href='#' onClick='bscraper()'> Build scraper (-10 bricks, -10 minerals)</a>"));
+	};
 };
+
+function automate() {
+    for (var i = 0; i < stash["scrapers"]; i++) {
+        scrape();
+    }
+    update();
+}
 
 function scrape(){	
 	stash["dust"]++;
@@ -56,24 +60,9 @@ function compact(){
 	update();
 };
 
-function refine(){
-	stash["minerals"] -= 10;
-	stash["metal"]++;
-	update();
-};
-
-function brobot() {
-	stash["metal"]--;
-	stash["robots"]++;
-	update();
-};
-
 function bscraper() {
-	stash["robots"]--;
+	stash["minerals"] -= 10;
 	stash["bricks"] -= 10;
 	stash["scrapers"]++;
-	setInterval(function(){
-		scrape();
-		update();
-	},1000);
+	update();
 };
